919135dba7acfb11e95e10c0bc1cb882b201e5b1
Hikmal Falah <ikmalfalah@gmail.com>
2023-09-03 02:33:02 +0700

    Membuat dan merapihkan folder css

--- auth/login.php
+++ auth/login.php
@@ -27,7 +27,7 @@ if( isset($_POST["login_btn"]) ) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login2.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <!-- Font Awesome -->
